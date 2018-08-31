68e06f783da68ceb1eb62e0c36c710e4d1900b71
Justin <JustinLi.LJT@gmail.com>
2018-08-31 15:59:44 +0800

    [MOD] Gradient Descent (for Multiple Variables) |   0 /   0 | Nice work!

--- machine-learning-ex1/ex1/gradientDescentMulti.m
+++ machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -16,42 +16,26 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
+    feature_num = size(X, 2);
+    next_theta = zeros(size(theta));
+    for itheta = 1:feature_num
+        % update one theta each iteration of itheta
+        sigma = 0;
 
-	% sigma = 0;
-	% for i = 1:m
-	% 	sigma = sigma + theta(1)*X(i,1) + theta(2)*X(i,2) +theta(3)*X(i,3) - y(i);
-	% end;
-	% theta_0 = theta(1) - alpha / m * sigma;
+        for i = 1:m
+            res = 0;
+            for j = 1:feature_num
+                res = res + theta(j)*X(i,j);
+            end;
+            res = (res - y(i))*X(i,itheta);
+            sigma += res;
+        end;
 
-	% sigma = 0;
-	% for i = 1:m
-	% 	sigma = sigma + (theta(1)*X(i,1) + theta(2)*X(i,2) +theta(3)*X(i,3) - y(i)) * X(i,2);
-	% end;
-	% theta_1 = theta(2) - alpha / m * sigma;
+        next_theta(itheta) = theta(itheta) - alpha/m*sigma;
+    
+    end;
 
-	% sigma = 0;
-	% for i = 1:m
-	% 	sigma = sigma + (theta(1)*X(i,1) + theta(2)*X(i,2) +theta(3)*X(i,3) - y(i)) * X(i,3);
-	% end;
-	% theta_2 = theta(3) - alpha / m * sigma;
-
-	% theta = [theta_0; theta_1; theta_2];
-
-	feature_num = size(X, 2);
-	for i = 1:m
-		res = 0;
-		% caculate H_theta_function_result
-		for j = 1:feature_num
-			res = res + theta(j)*X(i,j);
-		end;
-		res = res - y(i);
-		% update theta
-		for j = 1:feature_num
-			theta(j) = theta(j) -alpha / m * (res * X(i,j));
-		end;
-	end;
-
-
+    theta = next_theta;
     % ============================================================
 
     % Save the cost J in every iteration    
