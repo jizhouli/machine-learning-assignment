bd5a564134e2b75a05b391d216843f97251967e4
Justin <JustinLi.LJT@gmail.com>
2018-09-09 14:51:23 +0800

    Logistic Regression Cost and Gradient | Nice work

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -20,11 +20,30 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
+% calculate omega
+omega = 0.0;
+for i = 1:m
+    % calculate polynomial
+    poly = X(i,1)*theta(1) + X(i,2)*theta(2) + X(i,3)*theta(3);
+    % calculate sigmoid of polynomial
+    sigm = sigmoid(poly);
+    omega = omega + (y(i)*log(sigm) + (1-y(i))*log(1-sigm));
+end;
+% calculate cost
+J = omega*(-1)/m;
 
-
-
-
-
+for j = 1:length(theta)
+    % calculate omega
+    omega = 0.0;
+    for i = 1:m
+        % calculate polynomial
+        poly = X(i,1)*theta(1) + X(i,2)*theta(2) + X(i,3)*theta(3);
+        % calculate sigmoid of polynomial
+        sigm = sigmoid(poly);
+        omega = omega + (sigm-y(i))*X(i,j);
+    end;
+    grad(j) = omega/m;
+end;
 
 
 % =============================================================
